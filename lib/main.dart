c0d7b55ba3a3d02c4d7c1928b5bb294c53fe2f8f
kolade <kolademodupe007@gmail.com>
2023-12-13 11:33:54 +0100

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class TodoModel {
+
+  final String title;
+  bool completed;
+
+  TodoModel({required this.title, required this.completed});
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final List<TodoModel> _todoList = <TodoModel>[]; //an empty list of todos named _todoList, using the TodoModel as an arrangement/model for the todo lists.
+
+  final TextEditingController _textController = TextEditingController();
+
+  void _addTodoItem(String title){ //Here it says to add whatever the user inputs as a value(title), to the list of Todo. But to do this, it must be in the "TodoModel(title: title, completed: false)" format. 
+  setState(() {
+      _todoList.add(TodoModel(title: title, completed: false));
+    });
+    _textController.clear();
+    }
+
+  void todoCheckbox (TodoModel todo){
+    setState(() {
+      todo.completed = !todo.completed; 
+    });
+  }
+
+  void _deleteTodo(TodoModel todo) {
+    setState(() {
+      _todoList.removeWhere((element) => element.title == todo.title);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        children: _todoList.map((TodoModel todo) {
+          return ToDoItem(
+              todo: todo,
+              onTodoChanged: _handleTodoChange,
+              removeTodo: _deleteTodo);
+        }).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          _displayDialog();
+        },
+        tooltip: "Add Todo",
+        child: const Icon(Icons.add),
+      ), 
+      
+    );
+  }
+
+  Future<void> _displayDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add a todo'),
+          content: TextField(
+            controller: _textController,
+            decoration: const InputDecoration(hintText: 'Type your todo'),
+            autofocus: true,
+          ),
+          actions: <Widget>[
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _addTodoItem(_textController.text);
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class ToDoItem extends StatelessWidget {
+  ToDoItem(
+      {required this.todo,
+      required this.onTodoChanged,
+      required this.removeTodo})
+      : super(key: ObjectKey(todo));
+
+  final TodoModel todo;
+  final void Function(TodoModel todo) onTodoChanged;
+  final void Function(TodoModel todo) removeTodo;
+
+  TextStyle? _getTextStyle(bool checked) {
+    if (!checked) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onTodoChanged(todo);
+      },
+      leading: Checkbox(
+        checkColor: Colors.greenAccent,
+        activeColor: Colors.red,
+        value: todo.completed,
+        onChanged: (value) {
+          onTodoChanged(todo);
+        },
+      ),
+      title: Row(children: <Widget>[
+        Expanded(
+          child: Text(todo.title, style: _getTextStyle(todo.completed)),
+        ),
+        IconButton(
+          iconSize: 30,
+          icon: const Icon(
+            Icons.delete,
+            color: Colors.red,
+          ),
+          alignment: Alignment.centerRight,
+          onPressed: () {
+            removeTodo(todo);
+          },
+        ),
+      ]),
+    );
+  }
+}
